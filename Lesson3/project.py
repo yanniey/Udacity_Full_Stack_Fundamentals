7b57ee90f352c0f68f57c0c4fc98c2c8db842f95
Anyi Guo <yanniey.guo@gmail.com>
2015-07-24 08:44:36 -0500

    I'm stuck on this buildError thing for editMenuItem

--- Lesson3/project.py
+++ Lesson3/project.py
@@ -13,25 +13,39 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# decorator function
+
 @app.route('/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+
+    return output
+
+
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-	return render_template('menu.html',restaurant=restaurant,items = items)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
 
-	# this part is replaced by render_template() function above
-	# output = ''
-	# for i in items:
-	# 	output += i.name
-	# 	output += '</br>'
-	# 	output += i.price
-	# 	output += '</br>'
-	# 	output += i.description
-	# 	output += '</br></br>'
-
-	# return output
+    return output
 
 # Task 1: Create route for newMenuItem function here
 
@@ -47,9 +61,21 @@ def newMenuItem(restaurant_id):
 
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
-def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/edit',
+           methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, MenuID):
+    editedItem = session.query(MenuItem).filter_by(id=MenuID).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, MenuID=MenuID, i=editedItem)
 
 # Task 3: Create a route for deleteMenuItem function here
 
